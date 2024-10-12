a78e384cd233be05be2c29a3e9778771ca7eef59
Sana Jabeen <jsana7399@gmail.com>
2024-10-13 02:58:16 +0500

    image added

--- testgit/src/App.js
+++ testgit/src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import VinTest from "./VinTest";
 
 function App() {
   return (
     <div className="App">
+      <VinTest />
       <div>just for testing</div>
     </div>
   );
---
+++ testgit/src/VinTest.js
@@ -0,0 +1,13 @@
+import React from "react";
+import imagec from "./images/car-img.jpg";
+
+function VinTest() {
+  return (
+    <>
+      <div>vin-test</div>
+      <h1> welcome to APp</h1>
+    </>
+  );
+}
+
+export default VinTest;

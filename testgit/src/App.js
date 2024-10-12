dd0046d9bcec39fec38d88e711d5de017b128114
Sana Jabeen <jsana7399@gmail.com>
2024-10-13 02:27:54 +0500

    first commit

---
+++ testgit/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>just for testing</div>
+    </div>
+  );
+}
+
+export default App;
